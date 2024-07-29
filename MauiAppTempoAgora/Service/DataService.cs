c06977790dd1be13eaf3dc253aecf87f7282f91e
Usuario <Usuario@CPD03-M03>
2024-07-29 12:14:44 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/Service/DataService.cs
@@ -0,0 +1,20 @@
+﻿using MauiAppTempoAgora.Models;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MauiAppTempoAgora.Service
+{
+    public class DataService
+    {
+        public static async Task<Tempo?> GetPrevisaoDoTempo(string cidade)
+        {
+            // https://openweathermap.org/current#current_JSON
+            // https://home.openweathermap.org/api_keys
+            string appId = "6135072afe7f6cec1537d5cb08a5a1a2";
+
+            string url = $"http://openweathermap.org/data/2.5/weather?q=" +
+                         $"{cidade}";
+        }
+    }
+}
