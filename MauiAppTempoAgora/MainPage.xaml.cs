fa54c034429b21223145354491457199a46afb27
Usuario <Usuario@CPD03-M03>
2024-08-05 12:30:11 -0300

    edição

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -1,25 +1,56 @@
-﻿namespace MauiAppTempoAgora
+﻿using MauiAppTempoAgora.Models;
+using MauiAppTempoAgora.Service;
+using System.Diagnostics;
+
+
+namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        CancellationTokenSource _cancellationTokenSource;
+        bool _isCheckingLocation;
+
+        string? cidade;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
     }
 
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            _cancelTokenSource = new CancellationTokenSource();
+
+            GeolocationRequest request =
+                new GeolocationRequest(GeolocationAccuracy.Medium,
+                TimeSpan.FromSeconds(10));
+
+            Location? location = await Geolocation.Default.GetLocationAsync(
+                request, _cancelTokenSource.Token);
+
+            if (location != null)
+            {
+                lbl_latitude.Text = location.Latitude.ToString();
+                lbl_longitude.Text = location.Longitude.ToString();
+
+                Debug.WriteLine("_________________");
+                Debug.WriteLine(location);
+                Debug.WriteLine("_________________");
+
+            }
+        }
+
+        catch (FeatureNotEnabledException fnsEx)
+        {
+            await DisplayAlert("Erro: Dispositivo não suporta", fnsEx.Message, "OK");
+        }
+        catch (FeatureNotEnabledException fnsEx)
+        {
+            await DisplayAlert("Erro: Localização Desabilitada", fnsEx.Message, "OK");
+        }
+    }
 }
--- MauiAppTempoAgora/Service/DataService.cs
+++ MauiAppTempoAgora/Service/DataService.cs
@@ -1,7 +1,6 @@
 ﻿using MauiAppTempoAgora.Models;
-using System.Diagnostics;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
 namespace MauiAppTempoAgora.Service
 {
@@ -14,7 +13,49 @@ namespace MauiAppTempoAgora.Service
             string appId = "6135072afe7f6cec1537d5cb08a5a1a2";
 
             string url = $"http://openweathermap.org/data/2.5/weather?q=" +
-                         $"{cidade}";
+                         $"{cidade}&units=metri&appid={appId}";
+
+            Tempo tempo = null;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = response.Content.ReadAsStringAsync().Result;
+
+                    Debug.WriteLine("_________________");
+                    Debug.WriteLine(json);
+                    Debug.WriteLine("_________________");
+
+                    var rascunho = JObject.Parse(json);
+
+                    Debug.WriteLine("_________________");
+                    Debug.WriteLine(rascunho);
+                    Debug.WriteLine("_________________");
+
+                    DateTime time = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+                    DateTime sunrise = time.AddSeconds((double)rascunho["sys"]["sunrise"]).ToLocalTime();
+                    DateTime sunset = time.AddSeconds((double)rascunho["sys"]["sunset"]).ToLocalTime();
+
+                    tempo = new()
+                    {
+                        Humidity = (string)rascunho["main"]["humidity"],
+                        Temperature = (string)rascunho["main"]["temp"],
+                        Title = (string)rascunho["name"],
+                        Visibility = (string)rascunho["main"]["visibility"],
+                        Wind = (string)rascunho["wind"]["speed"],
+                        Sunrise = sunrise.ToString(),
+                        Sunset = sunset.ToString(),
+                        Weather = (string)rascunho["weather"][0]["main"],
+                        WeatherDescription = (string)rascunho["weather"][0]["description"],
+                    };
+
+                }
+            }
+
+            return tempo;
         }
     }
 }
