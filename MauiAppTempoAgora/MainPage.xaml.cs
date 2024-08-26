d7461c00ae69eb8f7928d133ee85d1c732a867b7
Usuario <Usuario@CPD03-M03>
2024-08-26 12:27:04 -0300

    edicao

--- MauiAppTempoAgora/MainPage.xaml.cs
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -7,7 +7,7 @@ namespace MauiAppTempoAgora
 {
     public partial class MainPage : ContentPage
     {
-        CancellationTokenSource _cancellationTokenSource;
+        CancellationTokenSource _cancelTokenSource;
         bool _isCheckingLocation;
 
         string? cidade;
@@ -17,40 +17,53 @@ namespace MauiAppTempoAgora
             InitializeComponent();
         }
 
-    }
 
-    private async void Button_Clicked(object sender, EventArgs e)
-    {
-        try
+
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-            _cancelTokenSource = new CancellationTokenSource();
-
-            GeolocationRequest request =
-                new GeolocationRequest(GeolocationAccuracy.Medium,
-                TimeSpan.FromSeconds(10));
-
-            Location? location = await Geolocation.Default.GetLocationAsync(
-                request, _cancelTokenSource.Token);
-
-            if (location != null)
+            try
             {
-                lbl_latitude.Text = location.Latitude.ToString();
-                lbl_longitude.Text = location.Longitude.ToString();
+                _cancelTokenSource = new CancellationTokenSource();
 
-                Debug.WriteLine("_________________");
-                Debug.WriteLine(location);
-                Debug.WriteLine("_________________");
+                GeolocationRequest request =
+                    new GeolocationRequest(GeolocationAccuracy.Medium,
+                    TimeSpan.FromSeconds(10));
 
+                Location? location = await Geolocation.Default.GetLocationAsync(
+                    request, _cancelTokenSource.Token);
+
+                if (location != null)
+                {
+                    lbl_latitude.Text = location.Latitude.ToString();
+                    lbl_longitude.Text = location.Longitude.ToString();
+
+                    Debug.WriteLine("_________________");
+                    Debug.WriteLine(location);
+                    Debug.WriteLine("_________________");
+
+                }
             }
-        }
 
-        catch (FeatureNotEnabledException fnsEx)
-        {
-            await DisplayAlert("Erro: Dispositivo não suporta", fnsEx.Message, "OK");
+            catch (FeatureNotEnabledException fnsEx)
+            {
+                await DisplayAlert("Erro: Dispositivo não suporta", fnsEx.Message, "OK");
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                await DisplayAlert("Erro: Localização Desabilitada", fneEx.Message, "OK");
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Erro: Permissão", pEx.Message, "OK");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro: ", ex.Message, "OK");
+            }
+
         }
-        catch (FeatureNotEnabledException fnsEx)
-        {
-            await DisplayAlert("Erro: Localização Desabilitada", fnsEx.Message, "OK");
-        }
+        
+        private async 
+        
     }
 }
